95d4957de8082a323bbdb44330a6564fcd48859a
Alex <alex@max12>
2025-12-05 13:03:29 +0100

    Primer commit

---
+++ ProyectoSignInApplication.java
@@ -0,0 +1,39 @@
+package proyectocrud;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import proyectocrud.ui.SignInController;
+
+/**
+ *
+ * @author alex
+ */
+public class ProyectoSignInApplication extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("ui/SignInFX.fxml"));
+        Parent root = (Parent)loader.load();
+        
+        SignInController controller = loader.getController();
+        
+        controller.initStage(stage,root);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
