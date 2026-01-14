0faea0cfb2e51ea4af8a47a6bb77a7c740a769c0
puchol <puchol@2DAM-010>
2026-01-14 10:36:25 +0100

    Subida de trabajo actual 14/01/2026 a las 10:36

--- ui/MovementController.java
+++ ui/MovementController.java
@@ -5,10 +5,31 @@
  */
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TableColumn;
+
 /**
  *
  * @author puchol
  */
 public class MovementController {
-    
+@FXML
+    private TableColumn date;
+@FXML
+    private TableColumn type;
+@FXML
+    private TableColumn amount;
+@FXML
+    private TableColumn balance;
+@FXML
+    private DatePicker filterDateFrom;
+@FXML
+    private DatePicker filterDateUntil;
+@FXML
+    private SplitMenuButton selectAccount;
+@FXML    
+    private Button buttonCancel;
 }
