c6382c18e17ca7025be42a66e6caccb66d08587e
puchol <puchol@2DAM-010>
2026-01-15 12:30:50 +0100

    Subida de trabajo actual 14/01/2026 a las 10:36

--- ui/MovementController.java
+++ ui/MovementController.java
@@ -5,17 +5,26 @@
  */
 package ui;
 
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Customer;
 
 /**
  *
  * @author puchol
  */
 public class MovementController {
+@FXML
+    private TextField idAccount;
 @FXML
     private TableColumn date;
 @FXML
@@ -25,11 +34,50 @@ public class MovementController {
 @FXML
     private TableColumn balance;
 @FXML
-    private DatePicker filterDateFrom;
+    private ComboBox comboBox;
 @FXML
-    private DatePicker filterDateUntil;
+    private TextField amountFormulario;
 @FXML
-    private SplitMenuButton selectAccount;
+    private Button buttonCreateMovement;
+@FXML
+    private Button buttonUndoLastMovement;
 @FXML    
     private Button buttonCancel;
+    
+    private static final Logger LOGGER = Logger.getLogger("ui/MovementController.ui");
+    private Customer customer;
+    
+    public void setCustomer(Customer customer) {
+            this.customer = customer;
+    }
+    
+    public void init(Stage stage, Parent root) {
+        LOGGER.info("Initializing Movement window");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styleSheet.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("Movement");
+        stage.setResizable(false);
+
+        //applyExit.setDisable(true);
+        // Asociar eventos a botones
+        buttonCreateMovement.setOnAction(this::handleButtonCreateMovementOnAction);
+        buttonUndoLastMovement.setOnAction(this::handleButtonUndoLastMovementOnAction);
+        buttonCancel.setOnAction(this::handleButtonCancelOnAction);
+
+
+        // Asociar validaciones a los campos
+
+        stage.show();
+    }
+     //private void handleButtonCreateMovementOnAction(ObservableValue observable, String oldValue, String newValue) {
+     //}
+     private void handleButtonCreateMovementOnAction(ActionEvent event) {
+     }
+     private void handleButtonUndoLastMovementOnAction(ActionEvent event) {
+     }
+     private void handleButtonCancelOnAction(ActionEvent event) {
+     }
+     
+
 }
