68ef6f4f655df7427d19aed0d5ca2c54f0187c18
AlexPuM4 <apuma3508@gmail.com>
2026-02-04 13:16:01 +0100

    Trabajo actual de la rama dev_Account

--- ui/MenuController.java
+++ ui/MenuController.java
@@ -7,8 +7,10 @@ import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 import java.awt.Desktop;
 import java.net.URI;
+import javafx.scene.control.MenuBar;
 
 public class MenuController {
+    @FXML private MenuBar menuBar;
     @FXML private Menu AboutMenu;
     @FXML private Menu HelpMenu;
     @FXML private Menu LogOutMenu;
@@ -28,15 +30,22 @@ public class MenuController {
     private void handleHelp() {
     try {
         java.net.URL url = getClass().getResource("/ui/help.html");
+        if (url != null) {
+            // DEBES añadir esta línea para que el navegador se abra
+            java.awt.Desktop.getDesktop().browse(url.toURI());
+        } else {
+            System.err.println("No se encontró el archivo ui/help.html");
+        }
     } catch (Exception e) {
         e.printStackTrace();
     }
     }
 
     private void handleLogOut() {
-        Stage stage = (Stage) AboutMenu.getParentPopup().getOwnerWindow();
-        if (stage != null) {
-            stage.close();
-        }
+    // Obtenemos la ventana (Stage) directamente desde el componente MenuBar
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    if (stage != null) {
+        stage.close();
+    }
     }
 }
