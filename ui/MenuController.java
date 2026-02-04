90295a3b77683e44eda59b42cde19cf900b5e41e
AlexPuM4 <apuma3508@gmail.com>
2026-02-04 12:58:09 +0100

    Trabajo actual de la rama dev_Account

--- ui/MenuController.java
+++ ui/MenuController.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ui;
 
-/**
- *
- * @author alex
- */
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
+import java.awt.Desktop;
+import java.net.URI;
+
 public class MenuController {
-    
+
+    @FXML private Menu AboutMenu;
+    @FXML private Menu HelpMenu;
+    @FXML private Menu LogOutMenu;
+
+    @FXML
+    public void initialize() {
+        AboutMenu.setOnShowing(event -> handleAbout());
+        HelpMenu.setOnShowing(event -> handleHelp());
+        LogOutMenu.setOnShowing(event -> handleLogOut());
+    }
+    private void handleAbout() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText(null);
+        alert.setContentText("Aplicación creada por Alex.");
+        alert.showAndWait();
+    }
+
+    private void handleHelp() {
+    try {
+        java.net.URL url = getClass().getResource("/ui/help.html");
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
+    private void handleLogOut() {
+        Stage stage = (Stage) AboutMenu.getParentPopup().getOwnerWindow();
+        if (stage != null) {
+            stage.close();
+        }
+    }
 }
